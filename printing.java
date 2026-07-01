90befb6cb644c7f8bd35d0bf815f428938537b40
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-01 16:37:12 +0530

    Printing the data from variables

--- printing.java
+++ printing.java
@@ -2,6 +2,10 @@ public class printing{
     public static void main(String[] args){
         String Name="Subhash";
         String Degree="B.TECH";
+        //printing without data
+        System.out.println(Name);
+        System.out.println(Degree);
+        //printing with data
         System.out.println("Name : "+Name);
         System.out.println("Degree : "+Degree);
     }
