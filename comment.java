7877bc4289ef1f9af396d6333016a9c6e59f4aaa
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-01 16:41:46 +0530

    Types of comments

---
+++ comment.java
@@ -0,0 +1,10 @@
+public class comment{
+    public static void main(String[] args){
+        System.out.println("Singleline comment");
+        //This is a single line comment
+        System.out.println("Multiline comment");
+        /*This is a multi line comment */
+        System.out.println("Java documentation");
+        /**This is java documentation */
+    }
+}
