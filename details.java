70c840153a83a56d631eb0b8ece1c711a801c8c1
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-01 16:31:57 +0530

    details

---
+++ details.java
@@ -0,0 +1,8 @@
+public class details{
+    public static void main(String[] args){
+        String Name="Subhash";
+        String Degree="B.TECH";
+        System.out.println("Name : "+Name);
+        System.out.println("Degree : "+Degree);
+    }
+}
