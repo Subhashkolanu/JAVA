5bc239e26d45083e515a3e0c541c8028dd08068a
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-01 17:42:06 +0530

    Progress card

---
+++ ProgressCard.java
@@ -0,0 +1,16 @@
+public class ProgressCard{
+    public static void main(String[] args){
+    String name="Krishna";
+    int age=18;
+    String college="VVITU";
+    double cgpa=8.0;
+    String discipline="CSE";
+    boolean passed=true;
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(cgpa);
+    System.out.println(college);
+    System.out.println(discipline);
+    System.out.println(passed);
+    }
+}
