c354f2e8e2c71781790f4f5eb88c0a6aa4a9829d
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-02 16:11:10 +0530

    Printing data on screen

--- printing.java
+++ printing.java
@@ -2,10 +2,10 @@ public class printing{
     public static void main(String[] args){
         String Name="Subhash";
         String Degree="B.TECH";
-        //printing without data
+        //printing without info
         System.out.println(Name);
         System.out.println(Degree);
-        //printing with data
+        //printing with info
         System.out.println("Name : "+Name);
         System.out.println("Degree : "+Degree);
     }
---
+++ variable.java
@@ -0,0 +1,8 @@
+public class variable{
+    public static void main(String[] args){
+        String name="Pranav";
+        int age=14;
+        System.out.println("Name : "+name);
+        System.out.println("Age : "+age);
+    }
+}
