5ec2a9e49ddfaf281cebe09a76abe4b1ca7e4072
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-10 19:06:51 +0530

    Area of rectangle

---
+++ rectanglearea.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class rectanglearea{
+    public static void main(String[] args) {
+        double length=0;
+        double breadth=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter length : ");
+        length=sc.nextDouble();
+        System.out.print("Enter Breadth : ");
+        breadth=sc.nextDouble();
+        System.out.println("Area of rectangle : "+length*breadth+"cm");
+    }
