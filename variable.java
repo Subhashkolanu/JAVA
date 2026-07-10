57fb2fdf41734e04844d3729f649164ff3a5da99
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-10 18:35:13 +0530

    Variables

--- variable.java
+++ variable.java
@@ -1,8 +1,17 @@
 public class variable{
     public static void main(String[] args){
-        String name="Pranav";
-        int age=14;
+        String name="BMW";
+        int yearOfManufacturing=2026;
+        float price=19999.99f;
+        double pickup=2.3;
+        char series='M';
+        boolean Gmake=true;
+        char currency='$';
         System.out.println("Name : "+name);
-        System.out.println("Age : "+age);
+        System.out.println("Series : "+series);
+        System.out.println("Pickup : "+pickup+"s");
+        System.out.println("German make : "+Gmake);
+        System.out.println("Year of manufacturing : "+yearOfManufacturing);
+        System.out.println("Price : " +currency+ price);
     }
 }
