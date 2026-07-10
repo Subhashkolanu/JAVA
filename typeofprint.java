2e023d3c6297d53d7c086cf4d4b896a214b5db02
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-10 10:57:29 +0530

    typesofprint

---
+++ typeofprint.java
@@ -0,0 +1,10 @@
+public class typeofprint{
+    public static void main(String[] args){
+        //Printing using System.out.print()
+        System.out.print("Infinix is the 5th largest mobile manufacturer");
+        System.out.print(" in the world\n" ); // This is used for printing data without moving to next line or continuing printing the data
+        //Printing using System.out.println()
+        System.out.println("Infinix is the 5th largest manufacturer");
+        System.out.println("Samsung is the 1st largest manufacturer");
+    }
+}
