990f77eb2b17301291b9c416f04e31bcdd53bb39
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-11 19:24:49 +0530

    Currenvy converter v1

--- currencyconverterv1.java
+++ currencyconverterv1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class currencyconverter{
+public class currencyconverterv1{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         double rupees=0;        
