2243c0f2c70efabc72db00e6a55d7a1bd463a580
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-11 07:49:57 +0530

    Cicle calculations

--- Circlecal.java
+++ Circlecal.java
@@ -3,13 +3,12 @@ public class Circlecal{
     public static void main(String[] args){
         double pi=3.1416;
         double radius=0;
-        double diameter=2*radius;
-        double area=0;
-        double circumference=0;
         Scanner scanner=new Scanner(System.in);
         System.out.print("Enter radius : ");
         radius=scanner.nextDouble();
         System.out.println("Area of circle : "+pi*radius*radius);
         System.out.println("Circumference of circle : "+2*pi*radius);
+        System.out.println("Diameter : "+2*radius);
+        scanner.close();
     }
 }
