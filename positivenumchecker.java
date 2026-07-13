299580b90cec533d8b9390eb7528d0a7797195da
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-13 20:36:15 +0530

    Positive number checker

---
+++ positivenumchecker.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class positivenumc{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        //Positive number checker:
+        int number;
+        System.out.print("Enter a number : ");
+        number=sc.nextInt();
+        if (number>0){
+            System.out.println("Positive number");
+        }
+        else if(number==0){
+            System.out.println("0 is the number");
+        }
+        else{
+            System.out.println("Negative number");
+        }
+    }
+}
