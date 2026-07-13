4250b0d48e5d72c955e2e2a036f40395b5842865
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-13 20:50:09 +0530

    largerst of two numbers

--- largestof2numbers.java
+++ largestof2numbers.java
@@ -1,22 +1,7 @@
 import java.util.Scanner;
-public class ifelse1{
+public class largestof2numbers{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-
-    //Pass or fail calculator
-    int marks;
-    System.out.print("Enter marks (max=100) : ");
-    marks=sc.nextInt();
-    if (marks>=75){
-        System.out.println("Distinction");
-    }
-    else if (marks>=35){
-        System.out.println("Passed");
-    }
-    else{
-        System.out.println("Failed");
-    }
-
     //Largest of two numbers
     int a;
     int b;
