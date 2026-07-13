08b1760860b90ddab0b9860571387c122b928132
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-13 20:12:20 +0530

    Added voting

--- ifelse1.java
+++ ifelse1.java
@@ -4,14 +4,27 @@ public class ifelse1{
         Scanner sc=new Scanner(System.in);
 
         //Positive number checker:
-        int number;
+        /*int number;
         System.out.print("Enter a number : ");
         number=sc.nextInt();
-        if (number>=0){
+        if (number>0){
             System.out.println("Positive number");
+        }
+        else if(number==0){
+            System.out.println("0 is the number");
         }
         else{
             System.out.println("Negative number");
-        }
+        }*/
+       int age;
+       System.out.print("Enter age : ");
+       age=sc.nextInt();
+       if (age>=18){
+        System.out.println("Eligible for voting!");
+       }
+       else{
+        System.out.println("Not eligible for voting");
+       }
+        
     }
 }
