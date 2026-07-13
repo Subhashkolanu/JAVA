99af31e419453b1c92bd4356941e36b42b42cadd
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-13 20:34:00 +0530

    Largest of two numbers

--- ifelse1.java
+++ ifelse1.java
@@ -40,7 +40,7 @@ public class ifelse1{
       }*/
 
     //Pass or fail calculator
-    int marks;
+    /*int marks;
     System.out.print("Enter marks (max=100) : ");
     marks=sc.nextInt();
     if (marks>=75){
@@ -51,7 +51,25 @@ public class ifelse1{
     }
     else{
         System.out.println("Failed");
+    }*/
+
+    //Largest of two numbers
+    int a;
+    int b;
+    System.out.print("Enter value for a : ");
+    a=sc.nextInt();
+    System.out.print("Enter value for b : ");
+    b=sc.nextInt();
+    if(a>b){
+        System.out.println("Value A is greater than Value of B");
     }
+    else if (a==b){
+        System.out.println("Both are equal");
+    }
+    else{
+        System.out.println("Value of B is greater than Value A");
+    }
+
     sc.close();
     }
 }
