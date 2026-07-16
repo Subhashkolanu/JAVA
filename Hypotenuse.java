03aec6d754ea71e524fd35d75647d7b12efd185c
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-16 20:35:41 +0530

    Hypotenuse

--- Hypotenuse.java
+++ Hypotenuse.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-
-import javax.sound.sampled.SourceDataLine;
 public class Hypotenuse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
