27a69129eb16032d0687b3adb482765892cfb13a
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-16 20:34:38 +0530

    Used Math methods

--- Circlecal.java
+++ Circlecal.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
-public class Circlecal{
+public class CircleCal{
     public static void main(String[] args){
+        //General methods
         double pi=3.1416;
-        double radius=0;
+        double radius;
         Scanner scanner=new Scanner(System.in);
         System.out.print("Enter radius : ");
         radius=scanner.nextDouble();
@@ -10,5 +11,13 @@ public class Circlecal{
         System.out.println("Circumference of circle : "+2*pi*radius);
         System.out.println("Diameter : "+2*radius);
         scanner.close();
+        //Using Math methods
+        double circumference = 2*Math.PI*radius;
+        System.out.printf("Circumference : %.1f\n",circumference);
+
+        double area = Math.PI*Math.pow(radius,2);
+        System.out.printf("Area : %.1f\n",area);
+        
+
     }
 }
