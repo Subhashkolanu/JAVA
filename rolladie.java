30a4e62b4f9082e825a0945cf6db88810f27cd00
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-16 16:15:32 +0530

    Roll a dice using random module

--- rolladie.java
+++ rolladie.java
@@ -3,6 +3,7 @@ public class rolladie {
     public static void main(String[] args){
         Random rd = new Random();
         int roll = rd.nextInt(6);
+        System.out.println("Rolling dice.....");
         System.out.println("Rolled : "+roll);
     }
 }
