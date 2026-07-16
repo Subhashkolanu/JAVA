d74ee2e48ce02d1761b7b7b92567f71ed2b9633a
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-16 16:13:16 +0530

    Roll a die

---
+++ rolladie.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+public class rolladie {
+    public static void main(String[] args){
+        Random rd = new Random();
+        int roll = rd.nextInt(6);
+        System.out.println("Rolled : "+roll);
+    }
+}
