08b89972b29e089265b3e14c95c9744a226d179f
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-16 13:24:11 +0530

    Random module

--- random.java
+++ random.java
@@ -8,9 +8,14 @@ public static void main(String[] args){
     n=rd.nextInt(); // Range from -2B to 2B
     System.out.println("Random integer : "+n);
     n=rd.nextInt(1,101); // Range from 1 to 100 (100 is inclusive)
+    System.out.println("Random integer : "+n);
     num = rd.nextDouble();
     System.out.println("Random double : "+num);
-    num = rd.nextDouble();
+    num = rd.nextDouble(1,101);
+    System.out.println("Random double : "+num);
+    isgood= rd.nextBoolean();
+     System.out.println("Random boolean : "+isgood);
+
 
 
 }
