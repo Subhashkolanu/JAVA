cdd3011bcc14443497d28395ae3e5a0ba7e6db9f
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-16 20:25:54 +0530

    Hypotenuse

---
+++ Hypotenuse.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+import javax.sound.sampled.SourceDataLine;
+public class Hypotenuse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a,b,c;
+        System.out.print("Enter the value of side A : ");
+        a = sc.nextDouble();
+        System.out.print("Enter the value of side B : ");
+        b = sc.nextDouble();
+
+        c=Math.sqrt(Math.pow(a,2)+Math.pow(b,2));
+        System.out.printf("Hypotenuse : %.1f",c);
+
+        sc.close();
+
+    }
+}
