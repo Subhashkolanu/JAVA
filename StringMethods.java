78af60522aaad74aaf0164b64db261d0931e1027
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-21 09:33:35 +0530

    String methods

--- StringMethods.java
+++ StringMethods.java
@@ -1,3 +1,13 @@
 public class StringMethods {
-    
+    public static void main(String[] args){
+        String name = "Infinix Note 50S 5G";
+        System.out.println(name.length());
+        System.out.println(name.charAt(2));
+        System.out.println(name.indexOf(0));
+        System.out.println(name.lastIndexOf("i"));
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.trim());
+        System.out.println(name.replace("I","i"));
+    }
 }
