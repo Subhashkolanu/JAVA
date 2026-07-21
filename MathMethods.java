24917233808ffb0be3237d37876c5eff5a9976fe
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-21 09:21:57 +0530

    Math methos

---
+++ MathMethods.java
@@ -0,0 +1,31 @@
+public class MathMethods {
+    public static void main(String[] args) {
+
+       System.out.println(Math.pow(2, 5)); // 2 raised to the power of 5
+       System.out.println(Math.abs(-5)); // Returns the absolute value
+       System.out.println(Math.sqrt(9)); // Returns the square root
+       System.out.println(Math.cbrt(27)); // Returns the cube root
+       System.out.println(Math.round(3.1548561)); // Rounds to the nearest whole number
+       System.out.println(Math.round(3.7845)); // Rounds to the nearest whole number
+       System.out.println(Math.ceil(9.1451)); // Rounds up to the next whole number
+       System.out.println(Math.floor(5.98522)); // Rounds down to the previous whole number
+       System.out.println(Math.max(10, 20)); // Returns the larger of two numbers
+       System.out.println(Math.min(485, 844)); // Returns the smaller of two numbers
+       System.out.println(Math.PI); //Pi value
+       System.out.println(Math.E); //Euler's value
+       System.out.println(Math.random()); //random btw 0.0 to 1.0
+       
+       // Trignometric functions
+
+       System.out.println(Math.sin(90));
+       System.out.println(Math.cos(90));
+       System.out.println(Math.tan(90));
+
+       // Log functions
+
+       System.out.println(Math.log(10));
+       System.out.println(Math.log10(15));
+       
+       System.out.println(Math.exp(5)); // e raised to power of 9
+    }
+}
