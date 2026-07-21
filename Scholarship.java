203c9498c1c523812be11accced613140cfd8379
SAI SUBHASH KOLANU <144505840+Subhashkolanu@users.noreply.github.com>
2026-07-21 08:55:20 +0530

    Scholarship

--- Scholarship.java
+++ Scholarship.java
@@ -1,26 +1,30 @@
-
 import java.util.Scanner;
 public class Scholarship {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         float percentage,attendance;
         int income;
-        System.out.println("Enter percentage : ");
+        System.out.print("Enter percentage : ");
         percentage=sc.nextFloat();
         if (percentage>=85){
-            System.out.println("Enter annual income : ");
+            System.out.print("Enter annual income : ");
             income=sc.nextInt();
             if (income<=300000){
-                System.out.println("Enter attendance :");
+                System.out.print("Enter attendance :");
                 attendance=sc.nextFloat();
                 if (attendance>=75){
                     System.out.println("Scholoarship approved");
                 }
                 else{
-                    System.out.println("");
+                    System.out.println("Attendance too low");
                 }
             }
-
+            else
+                System.out.println("Income limit exceedeed");
         }
+        else{
+            System.out.println("Not eligible");
+        }
+        sc.close();
     }
 }
