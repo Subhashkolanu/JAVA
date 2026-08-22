4edca9885f3d26dd01a98e4f15ecb87cc61bd05d
Sai Subhash Kolanu <2025DCSBT10480@vvitu.ac.in>
2026-08-22 20:49:42 +0530

    Simple calc

--- SimpleCalc.java
+++ SimpleCalc.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
-public class SimpleCalc{
-{
-	public static void main (String[] args)
-	{
-		// Update the blank in the code below
-		Scanner read = new Scanner(System.in);
-		int a = read.nextInt();
-		int b = read.nextInt() ;
-		int sum = a+b;
-		int diff = a-b;
-		System.out.println("Sum is: " + sum);
-		System.out.println("Difference is: " + diff);
+
+public class SimpleCalc {
+
+    public static void main(String[] args) {
+
+        Scanner read = new Scanner(System.in);
+
+        System.out.print("A: ");
+        int a = read.nextInt();
+
+        System.out.print("B: ");
+        int b = read.nextInt();
+
+        int sum = a + b;
+        int diff = a - b;
+
+        System.out.println("Sum is: " + sum);
+        System.out.println("Difference is: " + diff);
+
         read.close();
-	}
+    }
 }
