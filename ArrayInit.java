b6f1407b6b680fde9d7a44bf377bd04074f05f48
Sai Subhash Kolanu <2025DCSBT10480@vvitu.ac.in>
2026-09-14 09:10:10 +0530

    Array Init

---
+++ ArrayInit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ArrayInit{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int size;
+        System.out.print("Enter size of array : ");
+        size = sc.nextInt();
+        int a[] = new int[size];
+        for(int i = 0 ;i<size;i++){
+            a[i]=sc.nextInt();
+        }
+        sc.close();
+    }
+}
